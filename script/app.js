c41d7479187aa5806597ca9ec4748ab1bd6e513d
Haris Sajid <haris.sajid.2000@gmail.com>
2023-04-16 00:02:43 +0500

    Succed

---
+++ script/app.js
@@ -0,0 +1,22 @@
+// alert("Thanks for your input!");
+
+// var a="Haris ";
+// document.write(a);
+
+// var c=12;
+// var d=3;
+// document.write(c);
+
+// resultOfComputation = (2 * 4) * (4 + 2);
+// document.write(resultOfComputation);
+
+//  var message = "Thanks, ";
+//  user=prompt("Enter Your name ?")
+//   var banger = "!";
+//  alert(message + user + banger);
+
+Zakat=0.025;
+userZakatamount=prompt("Enter Your amount for Zakat Calculation");
+results=Zakat*userZakatamount;
+document.write(results);
+
